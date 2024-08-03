59f9729852abd4f21eaf2488c13c3d866a02ce46
Leon <tranhoangquan44@gmail.com>
2024-08-04 07:55:04 +1000

    remove redundant comments

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -52,14 +51,12 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
         err ? console.error(err) : console.log('README file created!')
     );
 }
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((responses) => {
         const markdown = generateMarkdown(responses);
